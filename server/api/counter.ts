0f2bb5ad6b3af3ddeaf1d94f49c1dfed422b6b08
eru123 <yeoligoakino@gmail.com>
2022-04-07 10:28:14 +0800

    setup firebase on the server

---
+++ server/api/counter.ts
@@ -0,0 +1,22 @@
+import { getFirestore } from 'firebase-admin/firestore'
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
+const apps = getApps()
+
+if (!apps.length) {
+    initializeApp({
+        credential: cert("service-account.json")
+    })
+}
+
+export default async (req, res) => {
+    const db = getFirestore()
+    const id = 'visits'
+    // increment the counter
+    const doc = await db.collection('counters').doc(id).get()
+    const counter = doc.data()
+    const newCount = (counter?.count || 0 )+ 1
+    await db.collection('counters').doc(id).set({ count: newCount })
+    // return the new count
+    return { count: newCount }
+}
+
