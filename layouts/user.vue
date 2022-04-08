9f9d72fae6b9829fe30d08c19fee7ad188c97190
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:51:46 +0800

    added user only page

---
+++ layouts/user.vue
@@ -0,0 +1,29 @@
+<script setup>
+if (process.client) {
+  const { auth } = useNuxtApp().$firebase()
+  auth.onAuthStateChanged(user => {
+    if (!user) {
+      navigateTo("/logout")
+    }
+  })
+}
+</script>
+<template>
+  <div class="wrapper">
+    <NuxtLink to="/user">User</NuxtLink>
+    <NuxtLink to="/logout">Logout</NuxtLink>
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.wrapper {
+  @apply text-center my-12;
+}
+a {
+  @apply px-4;
+
+  &.router-link-active {
+    @apply bg-gray-200;
+  }
+}
+</style>
