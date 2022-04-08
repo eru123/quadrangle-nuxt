c86ba4ba15414ce7741b8772fd15acd4b057cbc4
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:51:25 +0800

    set to blank layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,4 @@
-<script setup>
-
-if (process.client) {
-  const route = useRoute()
-  const { auth } = useNuxtApp().$firebase()
-  auth.onAuthStateChanged(user => {
-    if (user) {
-      navigateTo(route.query.redirect || "/user")
-    }
-  })
-}
-
-</script>
 <template>
-  <div class="wrapper">
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/register">Register</NuxtLink>
     <slot />
-  </div>
+  
 </template>
-<style lang="scss" scoped>
-.wrapper {
-  @apply text-center my-12;
-}
-a {
-  @apply px-4;
-
-  &.router-link-active {
-    @apply bg-gray-200;
-  }
-}
-</style>
