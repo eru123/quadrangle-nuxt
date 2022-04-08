7bfb63777e7b14e56dcd76ad6105b1db1b7d5db4
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:57:38 +0800

    update plugin to much practical method

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,19 +1,25 @@
 import { defineNuxtPlugin } from '#app'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth, } from 'firebase/auth'
+import { getAuth, signOut } from 'firebase/auth'
 
-export default defineNuxtPlugin(({$config}) => {
+export default defineNuxtPlugin(({ $config }) => {
   const app = initializeApp($config.FIREBASE);
 
   return {
     provide: {
-      firebase: () => ({
-        app,
-        firestore: getFirestore(app),
-        auth: getAuth(app),
-        logout: () => getAuth(app).signOut(),
-      })
+      firebase: () => {
+        const firestore = getFirestore(app)
+        const auth = getAuth(app)
+        const logout = () => signOut(auth)
+
+        return {
+          app,
+          firestore,
+          auth,
+          logout
+        }
+      }
     }
   }
 })
