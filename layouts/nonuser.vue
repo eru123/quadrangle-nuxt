fe49fae3b5cf5faad9ec6a3f8227629b56ceccb4
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:51:36 +0800

    added non user only page

---
+++ layouts/nonuser.vue
@@ -0,0 +1,31 @@
+<script setup>
+if (process.client) {
+  const route = useRoute()
+  const { auth } = useNuxtApp().$firebase()
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      navigateTo(route.query.redirect || "/user")
+    }
+  })
+}
+</script>
+<template>
+  <div class="wrapper">
+    <h1>Non User page</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/sign">Sign in</NuxtLink>
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.wrapper {
+  @apply text-center my-12;
+}
+a {
+  @apply px-4;
+
+  &.router-link-active {
+    @apply bg-gray-200;
+  }
+}
+</style>
