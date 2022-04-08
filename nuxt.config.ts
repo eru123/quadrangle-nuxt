1e39e9af345fcac1b1ff051439fd7239b5394217
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:23:01 +0800

    added firebase runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    FIREBASE: {
+      apiKey: process.env.FBC_apiKey,
+      authDomain: process.env.FBC_authDomain,
+      databaseURL: process.env.FBC_databaseURL,
+      projectId: process.env.FBC_projectId,
+      storageBucket: process.env.FBC_storageBucket,
+      messagingSenderId: process.env.FBC_messagingSenderId,
+      appId: process.env.FBC_appId,
+      measurementId: process.env.FBC_measurementId
+    }
+  },
   build: {
     postcss: {
       postcssOptions: {
