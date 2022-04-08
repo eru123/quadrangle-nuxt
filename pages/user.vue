76bf18a45e132b8e8fd9577337f6c53c9fb1c446
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:52:23 +0800

    added user page

---
+++ pages/user.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>User page</h1>
+  </div>
+</template>
+<script>
+definePageMeta({
+  layout: "user",
+});
+</script>
+<style lang='scss' scoped>
+  
+</style>
