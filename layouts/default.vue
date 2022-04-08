e3d025574a2536a877d978cdd016c177e24d518d
eru123 <yeoligoakino@gmail.com>
2022-04-08 19:23:27 +0800

    added firebase auth watcher

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,32 @@
+<script setup>
+
+if (process.client) {
+  const route = useRoute()
+  const { auth } = useNuxtApp().$firebase()
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      navigateTo(route.query.redirect || "/user")
+    }
+  })
+}
+
+</script>
 <template>
-  <div>
-    <h1>Some shared layout content:</h1>
+  <div class="wrapper">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/register">Register</NuxtLink>
     <slot />
   </div>
 </template>
-<style lang="scss">
-h1 {
-  @apply text-green-500;
+<style lang="scss" scoped>
+.wrapper {
+  @apply text-center my-12;
+}
+a {
+  @apply px-4;
+
+  &.router-link-active {
+    @apply bg-gray-200;
+  }
 }
 </style>
