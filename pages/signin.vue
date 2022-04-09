c11bf9efe3c83f1b0159f9a05678651c15003771
eru123 <yeoligoakino@gmail.com>
2022-04-09 17:19:57 +0800

    added sign in page

---
+++ pages/signin.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+
+const accountType = useAccountType();
+
+</script>
+<template>
+  <div class="w">
+    <div class="c b">
+      <div class="c">
+        <img src="@/assets/images/signup.png" />
+        <h1>
+          Learn with your
+          <span class="brand">Quadmates</span>
+        </h1>
+        <p>Find like-minded individuals and collaborate, teach and learn from each other</p>
+      </div>
+    </div>
+    <div class="c f">
+      <div class="c">
+        <img src="@/assets/images/logo.png" alt="" class="logo">
+        <h1>Sign In</h1>
+        <h3>Choose account type</h3>
+        <ChooseAccountType />
+        <div class="actions">
+          <button>Sign in with Google</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang='scss' scoped>
+.w {
+  @apply h-screen w-screen flex flex-row;
+
+  & > .c {
+    @apply flex-1 p-4 flex flex-col items-center justify-center;
+
+    & > .c {
+      width: 80%;
+      max-width: 500px;
+      @apply flex flex-col items-center justify-center text-center;
+    }
+
+    &.b {
+      @apply bg-green-100;
+
+      img {
+        width: 100%;
+        max-width: 300px;
+      }
+
+      h1 {
+        @apply text-3xl font-bold text-gray-800 pt-12 pb-4;
+        font-family: "Montserrat", sans-serif;
+        .brand {
+          @apply font-bold text-teal-500;
+        }
+      }
+      p {
+        @apply font-bold text-gray-700 text-lg;
+      }
+    }
+
+    &.f {
+      @apply bg-white;
+
+      & > .c {
+        max-width: 400px;
+      }
+
+      .logo {
+        width: 5rem;
+      }
+
+      h1 {
+        @apply text-2xl font-bold text-gray-800 pt-12;
+      }
+      h3 {
+        @apply text-sm font-bold text-gray-700 mb-4;
+      }
+
+      .actions {
+        @apply flex flex-row items-center justify-center mt-8;
+        button {
+          @apply px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 transition-all duration-200 ease-in-out;
+        }
+      }
+    }
+  }
+}
+</style>
