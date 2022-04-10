24df95e40e109d8a53654e6f087ba30308eca68f
eru123 <yeoligoakino@gmail.com>
2022-04-10 12:48:05 +0800

    implement loading screen

--- app.vue
+++ app.vue
@@ -1,7 +1,22 @@
+<script setup>
+const appState = useAppState()
+const userState = useUserState()
+
+if (process.client) {
+  appState.value = 'loading'
+  const { auth } = useNuxtApp().$firebase()
+  auth.onAuthStateChanged(user => {
+    appState.value = 'ready'
+    userState.value = user
+  })
+}
+
+</script>
 <template>
-  <NuxtLayout>
+  <NuxtLayout v-if="appState == 'ready'">
     <NuxtPage />
   </NuxtLayout>
+  <LoadingScreen v-if="appState == 'loading'" />
 </template>
 <style lang='scss'>
 @import "assets/styles/tailwind.scss";
