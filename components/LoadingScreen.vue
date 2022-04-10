526eab1dbac8b96da5dfc3a51d0c3cf2b00a265d
eru123 <yeoligoakino@gmail.com>
2022-04-10 12:52:12 +0800

    added loading screen

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w">
+    <div class="lds-hourglass"></div>
+  </div>
+</template>
+<style lang='scss' scoped>
+.w {
+  @apply fixed h-screen w-screen flex flex-row items-center justify-center;
+  background-color: rgba(255, 255, 255, 0.7);
+  .lds-hourglass {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-hourglass:after {
+    content: " ";
+    display: block;
+    border-radius: 50%;
+    width: 0;
+    height: 0;
+    margin: 8px;
+    box-sizing: border-box;
+    border: 32px solid #40be71;
+    border-color: #40be71 transparent #40be71 transparent;
+    animation: lds-hourglass 1.2s infinite;
+  }
+  @keyframes lds-hourglass {
+    0% {
+      transform: rotate(0);
+      animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+    50% {
+      transform: rotate(900deg);
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+    100% {
+      transform: rotate(1800deg);
+    }
+  }
+}
+</style>
