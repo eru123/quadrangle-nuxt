4f0531ddd3dbb12bfc70c2615602484c1b714aa6
eru123 <yeoligoakino@gmail.com>
2022-04-10 17:16:15 +0800

    rename

---
+++ layouts/visitor.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { watchEffect } from 'vue'
+
+if (process.client) {
+  const route = useRoute()
+  const userState = useUserState()
+  watchEffect(() => {
+    if (userState.value) {
+      navigateTo(route.query.redirect || "/")
+    }
+  })
+}
+
+</script>
+<template>
+  <div class="w-full">
+    <LandingHeader />
+    <slot />
+  </div>
+</template>
