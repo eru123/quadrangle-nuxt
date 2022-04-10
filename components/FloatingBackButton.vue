cc01783af4de46a176931278b6a5ac9453df4e5b
eru123 <yeoligoakino@gmail.com>
2022-04-10 18:33:58 +0800

    update text color

--- components/FloatingBackButton.vue
+++ components/FloatingBackButton.vue
@@ -3,7 +3,7 @@
 </template>
 <style lang='scss' scoped>
   .b {
-    @apply fixed top-4 left-4 hover:bg-teal-500 hover:text-white p-4 rounded-full m-0 transition-all duration-200 ease-in-out;
+    @apply fixed top-4 left-4 text-gray-700 hover:bg-teal-500 hover:text-white p-4 rounded-full m-0 transition-all duration-200 ease-in-out;
     font-size: 1em;
     line-height: 1em;
   }
