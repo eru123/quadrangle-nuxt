55e72bafcc49086d63851b4450a8ffe37c07bc0f
eru123 <yeoligoakino@gmail.com>
2022-04-10 14:23:19 +0800

    added new states

--- composables/states.ts
+++ composables/states.ts
@@ -1,3 +1,7 @@
 export const useAccountType = () => useState<string>('accountType', () => 'none');
+export const useAccountTypeList = () => useState<any>('AccountTypeList', () => [
+  { title: 'quadmate', emoji: '👨‍🎓', description: 'Find Career mentors, subject tutors, and like-minded individuals' },
+  { title: 'quadmaster', emoji: '👨‍🏫', description: 'Share your knowledge and expertise as a mentor' },
+])
 export const useAppState = () => useState<string>('appState', () => 'loading');
-export const useUserState = () => useState<string>('userState', () => null);
+export const useUserState = () => useState<any>('userState', () => null);
