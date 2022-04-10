9314de8895433e7429543ebb99d6f7b27a5d112b
eru123 <yeoligoakino@gmail.com>
2022-04-10 12:48:23 +0800

    added app and user state

--- composables/states.ts
+++ composables/states.ts
@@ -1,1 +1,3 @@
 export const useAccountType = () => useState<string>('accountType', () => 'none');
+export const useAppState = () => useState<string>('appState', () => 'loading');
+export const useUserState = () => useState<string>('userState', () => null);
