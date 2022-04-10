9b4fb018f08f5b819d51a65f2e03a8a41be35b53
eru123 <yeoligoakino@gmail.com>
2022-04-10 18:25:40 +0800

    update color scheme

---
+++ components/FloatingBackButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLink class="b" to="/">🡸</NuxtLink>
+</template>
+<style lang='scss' scoped>
+  .b {
+    @apply fixed top-4 left-4 hover:bg-teal-500 hover:text-white p-4 rounded-full m-0 transition-all duration-200 ease-in-out;
+    font-size: 1em;
+    line-height: 1em;
+  }
+</style>
